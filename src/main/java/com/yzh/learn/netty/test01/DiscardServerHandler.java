8b4af79004db4e655b236419a54a419c179acf5d
wb-yzh737364 <wb-yzh737364@alibaba-inc.com>
2020-05-18 16:01:23 +0800

    yzh test

---
+++ src/main/java/com/yzh/learn/netty/test01/DiscardServerHandler.java
@@ -0,0 +1,25 @@
+package com.yzh.learn.netty.test01;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            System.out.println(in.toString(io.netty.util.CharsetUtil.US_ASCII));
+        } finally {
+            ReferenceCountUtil.release(msg); // (2)
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
